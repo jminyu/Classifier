99ceb6fa9d6473b65cfc4dbc22bbe426f80dd493
jongmin yu <jmyu@gist.ac.kr>
2014-09-04 15:24:55 +0900

    upload today

--- Hidden Markov Models/HMMs.py
+++ Hidden Markov Models/HMMs.py
@@ -1,19 +1,8 @@
 __author__ = 'Schmidtz'
 
-import numpy as np
-from sklearn import hmm
-import matplotlib.pyplot as plt
-
-startprob = np.array([0.6, 0.3, 0.1])
-transmat = np.array([[0.7, 0.2, 0.1], [0.3, 0.5, 0.2], [0.3, 0.3, 0.4]])
-means = np.array([[0.0, 0.0], [3.0, -3.0], [5.0, 10.0]])
-covars = np.tile(np.identity(2), (3, 1, 1))
-model = hmm.GaussianHMM(3, "full", startprob, transmat)
-model.means_ = means
-model.covars_ = covars
-X, Z = model.sample(100)
-
-plt.figure(1)
-plt.plot(X[:,0],X[:,1])
-plt.show()
-
+class GeneralHMMs():
+    def __init__(self,start_prob=None, transition_mtx=None, obsevation_prob=None,algorithm="viterbi"):
+        self.start_prob = start_prob
+        self.transision_mtx = transition_mtx
+        self.observation_prob = obsevation_prob
+        self.algorithm = algorithm
