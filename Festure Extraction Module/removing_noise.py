264b2bb64ff49a2372923dbeab38a980ef098419
jongmin yu <jmyu@gist.ac.kr>
2014-09-05 23:08:23 +0900

    HOF upload

--- Festure Extraction Module/removing_noise.py
+++ Festure Extraction Module/removing_noise.py
@@ -21,7 +21,7 @@ def hog_extraction(frame):
     :return: hog dat(array) and hog_image(shown for people)
     """
     gray_frame = color.rgb2gray(frame)
-    hog_dat,hog_image = hog(gray_frame,orientations=8,pixels_per_cell=(8,8),cells_per_block=(1,1),visualise=True)
+    hog_dat,hog_image = hog(gray_frame,orientations=16,pixels_per_cell=(40,40),cells_per_block=(2,2),visualise=True)
     return hog_dat,hog_image
 
 
@@ -50,8 +50,8 @@ def MorpologyClose(cap_stream,morp_stream,HOG_stream,csv_file_name):
             csv_writer.writerow(hog_dat)
 
             ##showing hog image session
-            #hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
-            #cv2.imshow('frame',hog_image_rescaled)
+            hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+            cv2.imshow('frame',hog_image_rescaled)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
